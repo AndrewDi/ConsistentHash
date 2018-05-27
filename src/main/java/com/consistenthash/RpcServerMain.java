997e6dfa50ee9e8deb228fc7d8f1c05adbc34bc9
Andrew <pandipd@outlook.com>
2018-05-27 12:22:30 +0800

    Basic rpc framework build

--- src/main/java/com/consistenthash/RpcServerMain.java
+++ src/main/java/com/consistenthash/RpcServerMain.java
@@ -1,15 +1,1 @@
-package com.consistenthash;
-
-import com.consistenthash.framework.RpcClient;
-import com.consistenthash.framework.RpcServer;
-
-public class RpcServerMain {
-
-    public static void main(String[] args) throws Exception {
-        RpcServer rpcServer = new RpcServer("127.0.0.1",12345);
-        rpcServer.start();
-        ITest test=new Test();
-        rpcServer.putClass(ITest.class.getName(),test);
-        rpcServer.waitCore();
-    }
-}
+package com.consistenthash;import com.consistenthash.framework.RpcClient;import com.consistenthash.framework.RpcServer;public class RpcServerMain {    public static void main(String[] args) throws Exception {        RpcServer rpcServer = new RpcServer("127.0.0.1",12345);        rpcServer.start();        ITest test=new Test();        rpcServer.putClass(ITest.class.getName(),test);        rpcServer.waitCore();    }}
